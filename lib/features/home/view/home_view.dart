af5d067519778457c9c80ce13753640746da1b6d
VRMX2 <lehcengrissi@gmail.com>
2025-11-18 04:30:54 -0700

    new commit

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+import 'package:hungry/core/constants/app_colors.dart';
+import 'package:hungry/shared/custom_text.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            Gap(75),
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/logo/logo.svg',
+                      color: AppColors.primary,
+                      height: 35,
+                    ),
+                    Gap(5),
+                    CustomText(
+                        text: 'Hello,Grissi Lahcen'
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
